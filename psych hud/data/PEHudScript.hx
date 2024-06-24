b34d7fa0ed654a01c4c5cd20d91dc682cc11d1a2
[504]brandon <97409501+504brandon@users.noreply.github.com>
2024-06-23 20:37:58 -0500

    ohh wow a PE UI

---
+++ psych hud/data/PEHudScript.hx
@@ -0,0 +1,50 @@
+import PlayState;
+import Conductor;
+import HelperFunctions;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxStringUtil;
+
+var state;
+var didUIShit = false;
+
+function create() {
+    state = FlxG.state;
+
+    if (PlayState.instance != null && state == PlayState.instance) {
+        trace("PsychUI");
+
+        for (obj in [state.songPosBG, state.songPosBar, state.songName])
+            obj.alpha = 0;
+
+        state.kadeEngineWatermark.visible = false;
+
+        state.scoreTxt.size = 20;
+    }
+}
+
+function startSong() {
+    state.songPosBG.loadGraphic(Paths.image("timeBar"));
+    state.songPosBG.screenCenter();
+    state.songPosBar.scale.x = 0.66;
+    state.songPosBar.createFilledBar(0xff000000, 0xFFffffff);
+
+    for (obj in [state.songPosBG, state.songPosBar, state.songName]) {
+        obj.alpha = 0;
+
+        FlxTween.tween(obj, {alpha: 1}, 0.5);
+    }
+
+    state.songName.size = 32;
+    state.songPosBG.y = state.songName.y + (state.songName.height / 4);
+    state.songPosBar.y = state.songPosBG.y + 4;
+}
+
+function updatePost(elapsed) {
+    if (PlayState.instance != null && state == PlayState.instance) {
+        state.scoreTxt.text = "Score: " + state.songScore + " | Misses: " + state.misses + " | Accuracy: " + HelperFunctions.truncateFloat(state.accuracy, 2) + "%";
+        state.scoreTxt.screenCenter();
+        state.scoreTxt.y = healthBar.y + 40;
+
+        state.songName.text = FlxStringUtil.formatTime((FlxG.sound.music.length - Conductor.songPosition) / 1000);
+    }
+}
